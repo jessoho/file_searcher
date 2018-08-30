f252b279dd0deae195c0419181519a359309a6c5
Jessica Otto <jessica.c.otto@gmail.com>
2018-08-30 17:46:32 +0200

    script up and running

--- f_s_4.rb
+++ f_s_4.rb
@@ -1,16 +1,19 @@
-folders = Dir["C:/Users/jessi/Documents/**"]
-files = Dir["C:/Users/jessi/Documents/**/*.*"]
-results = folders - files
 
+puts ""
+puts "Hi there! I will help you to search through a folder of your choice.
+I will show you, which folders contain files and which ones are empty."
+puts ""
 puts "Which folder should be looked through?"
-input_folder = gets.chomp.to_i
-
+puts "Please follow this structure 'drive:/path/folder'"
+puts ""
+input_folder = Dir["#{gets.chomp}/**"]
+puts ""
 puts "Ok we search through #{input_folder}"
-
-results.each do |folder|
-    if Dir["#{folder}/*.*"] != []
-      puts folder
-      puts "number of files found:"
-      puts Dir["#{folder}/*.*"].count
+puts ""
+input_folder.each do |item|
+    if Dir["#{item}/*.*"] != []
+      puts item
+      puts "number of files found:" + Dir["#{item}/*.*"].count.to_s
+      puts ""
     end
 end
