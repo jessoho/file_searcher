fd39552eae0b60cc0ef3943269cc4ec0370bbd1f
Jessica Otto <jessica.c.otto@gmail.com>
2018-08-31 11:24:47 +0200

    Revert "Count does work now"
    This reverts commit ff3edb05f27809aee0de6aed8f4871db758715b6.

--- f_s_4.rb
+++ f_s_4.rb
@@ -1,20 +1,18 @@
 
 puts ""
 puts "Hi there! I will help you to search through a folder of your choice.
-I will show you, which folders contain files. I won't bore you with folders that are empty."
+I will show you, which folders contain files and which ones are empty."
 puts ""
 puts "Which folder should be looked through?"
 puts "Please follow this structure 'drive:/path/folder'"
 puts ""
-folder_count = 0
 input_user = gets.chomp
-input_folder = Dir["#{input_user}/**/*"]
+input_folder = Dir["#{input_user}/**"]
 puts ""
 puts "Ok, we searched through: #{input_user}"
 puts ""
 input_folder.each do |item|
     if Dir["#{item}/*.*"] != []
-      folder_count = folder_count+1
       puts item
       puts "Number of files found: " + Dir["#{item}/*.*"].count.to_s
       puts "Name of file(s):"
@@ -22,4 +20,3 @@ input_folder.each do |item|
       puts ""
     end
 end
-puts "Number of folders with files found: " + folder_count.to_s
