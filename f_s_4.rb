eb4fd49be8fbe6ffcefe43b8b589b4935c1d672b
Jessica Otto <jessica.c.otto@gmail.com>
2018-08-31 12:15:21 +0200

    Text change intro

--- f_s_4.rb
+++ f_s_4.rb
@@ -4,6 +4,7 @@ puts "Hi there! I will help you to search through a folder of your choice.
 I will show you, which subfolders contain files. I won't bore you with folders that are empty."
 puts ""
 puts "Which folder should be looked through? Just insert the path."
+puts "Be patient please, this may take a while."
 puts ""
 folder_count = 0
 input_user = gets.chomp
